cde4084cd977ab6877ec4dcb68b5b72363a21b63
Emme Menezes <menezes.ecd@gmail.com>
2024-11-04 20:37:03 -0300

    Extende exibição para eventos de vários dias

--- frontend/packages/portalbrasil-intranet/src/components/Blocks/Calendario/View.jsx
+++ frontend/packages/portalbrasil-intranet/src/components/Blocks/Calendario/View.jsx
@@ -6,20 +6,33 @@ import withQuerystringResults from './withQuerystringResults';
 
 const groupByDate = (items) => {
   return items.reduce((map, obj) => {
-    const key = obj.start ? parseDate(obj.start.slice(0, 10)).toString() : null;
-    if (key) {
-      if (map[key] === undefined) {
-        map[key] = [];
+    if (!obj.start) return;
+
+    let start = obj.start ? parseDate(obj.start.slice(0, 10)) : null;
+    const end = obj.end ? parseDate(obj.end.slice(0, 10)) : start;
+
+    while (start < end) {
+      const key =  start.toString();
+
+      if (key) {
+        if (map[key] === undefined) {
+          map[key] = [];
+        }
+        map[key].push(obj);
       }
-      map[key].push(obj);
+
+      start = start.add({days: 1})
     }
     return map;
+    
+
   }, {});
 };
 
 const CalendarioBlockView = withQuerystringResults((props) => {
   const { data, isEditMode, path, pathname, className, listingItems } = props;
   const items = listingItems ? groupByDate(listingItems) : {};
+  
   return (
     <CalendarioView
       data={data}
